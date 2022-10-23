e2cf215f4050df05bcab4f85b36353839092abae
Jerry <jerry940100@gmail.com>
2022-10-24 00:38:06 +0800

    database

---
+++ db/create_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Patient (
+    PatientID int NOT NULL,
+    PatientName varchar(50) NOT NULL,
+    PatientSex varchar(50) NOT NULL,
+    PatientBirthdate date NOT NULL,
+    PRIMARY KEY (PatientID)
+);
+CREATE TABLE Image (
+    ImageID int NOT NULL,
+    PatientID int NOT NULL,
+    ImageDate date NOT NULL,
+    AiType varchar(50) NOT NULL,
+    ImagePath varchar(50) NOT NULL,
+    Report varchar(100) NOT NULL,
+    Finish boolean NOT NULL,
+    PRIMARY KEY (ImageID),
+    FOREIGN KEY (PatientID) REFERENCES Patient(PatientID)
+);
