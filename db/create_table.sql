94945f972a08fb59daa1dfcae069866aceb9e105
Jerry <jerry940100@gmail.com>
2022-10-25 17:15:42 +0800

    update table

--- db/create_table.sql
+++ db/create_table.sql
@@ -9,7 +9,7 @@ CREATE TABLE Image (
     ImageID int NOT NULL,
     PatientID int NOT NULL,
     ImageDate datetime NOT NULL,
-    AiType varchar(50) NOT NULL,
+    AIType varchar(50),
     ImagePath varchar(50) NOT NULL,
     Report varchar(100) NOT NULL,
     Finish boolean NOT NULL,
