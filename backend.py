22ca643b0f90b37ed67a4ebdb929e39291f4eee2
jerry940100 <jerry940100@gmail.com>
2022-10-31 16:21:39 +0800

    add CORS

--- backend.py
+++ backend.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from connectdb import *
 from parse import *
 
 app = Flask(__name__)
+CORS(app)
+
+
 @app.route("/get_patient", methods=["GET"])
 def get_patient():
     patient_id = request.args.get('patient_id')
     patient_rows = db.sql_fetch_patient(patient_id)
     patient_data = parse_patient(patient_rows)
     return jsonify(patient_data)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
