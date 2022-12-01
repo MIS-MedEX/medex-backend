1dbc6786260978434b429d40496c98995db67de2
jerry940100 <jerry940100@gmail.com>
2022-12-01 18:20:08 +0800

    update db

---
+++ db/insert_patient_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO Patient(ID, Name, Birthdate, Sex) 
+VALUES 
+    (1, 'Angelina',  '2011-09-31', 'Female'),
+    (2, 'Monica',  '1999-04-15', 'Female'),
+    (3, 'Jonathan',  '1991-07-25', 'Male'),
+    (4, 'Eric',  '1972-11-20', 'Male'),
+    (5, 'Patric',  '1981-06-09', 'Male');
